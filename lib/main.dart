058c4318329da32b19bcd75182192c8389796a18
ABHISHEK PATHAK <pathakabhishek0003@gmail.com>
2019-11-15 20:02:02 +0530

    created fully fledged music player app

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,14 @@ void main() => runApp(MyApp());
 
 Expanded playButton({Color color, int someAudioNumber}) {
   return Expanded(
-    child: FlatButton(
-      onPressed: () {
-        final player = AudioCache();
-        player.play('note$someAudioNumber.wav');
-      },
-      child: Container(
+    child: Container(
         color: color,
-      ),
-    ),
+        child: FlatButton(
+          onPressed: () {
+            AudioCache player = AudioCache();
+            player.play('note$someAudioNumber.wav');
+          },
+        )),
   );
 }
 
@@ -23,17 +22,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        appBar: AppBar(backgroundColor: Colors.purple),
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text('XyloPhone'),
+        ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             playButton(color: Colors.red, someAudioNumber: 1),
             playButton(color: Colors.green, someAudioNumber: 2),
-            playButton(color: Colors.pink, someAudioNumber: 3),
+            playButton(color: Colors.grey, someAudioNumber: 3),
             playButton(color: Colors.blue, someAudioNumber: 4),
-            playButton(color: Colors.pinkAccent, someAudioNumber: 5),
-            playButton(color: Colors.greenAccent, someAudioNumber: 6),
-            playButton(color: Colors.redAccent, someAudioNumber: 7),
+            playButton(color: Colors.purple, someAudioNumber: 5),
+            playButton(color: Colors.orange, someAudioNumber: 6),
+            playButton(color: Colors.pink, someAudioNumber: 7),
           ],
         ),
       ),
