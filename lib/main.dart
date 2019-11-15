f648015c604acec39316ba33b959399cbe30789f
ABHISHEK PATHAK <pathakabhishek0003@gmail.com>
2019-11-15 18:51:25 +0530

    created a resuable musical button widtget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+Expanded playButton({Color color}) {
+  return Expanded(
+    child: FlatButton(
+      onPressed: () {
+        // play sound
+      },
+      child: Container(
+        color: color,
+      ),
+    ),
+  );
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,14 +25,13 @@ class MyApp extends StatelessWidget {
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                onPressed: () {
-                  // play sound
-                },
-                color: Colors.red,
-              ),
-            )
+            playButton(color: Colors.red),
+            playButton(color: Colors.green),
+            playButton(color: Colors.pink),
+            playButton(color: Colors.blue),
+            playButton(color: Colors.pinkAccent),
+            playButton(color: Colors.greenAccent),
+            playButton(color: Colors.redAccent),
           ],
         ),
       ),
